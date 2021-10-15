b26da9c872ac85ae4ec2594c11e3727382472e91
ItsTsunamix <83253758+ItsTsunamix@users.noreply.github.com>
2021-10-15 19:11:12 -0400

    Update MoonLibrary.lua

--- MoonLibrary.lua
+++ MoonLibrary.lua
@@ -1,1 +1,31 @@
-warn("Library seems to be *very* bugged right now, please wait for an update until further notice");
+--Written by Tsunamix/Proxide
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/ItsTsunamix/UiLibrary/main/Moon.lua", true))();
+-- //////////////////////////////////////////////////////////////////////
+local libraryname = library:Create("Moon Library"); --name of lib
+
+local main = library:CreateTab("Features") --tab
+
+main:CreateLabel("Library made by Tsunamix/Proxide") --creates a label
+
+local dropdown = main:CreateDropdown("Dropdown") --creates dropdown (kinda buggy)
+
+dropdown:AddButton("Dropdown Option", function() --unlike any other libraries, moonlibrary uses another method
+	print("You clicked on the dropdown option.")
+end)
+
+dropdown:AddButton("Dropdown Option 2", function()
+	print("You clicked on the second dropdown option.")
+end)
+
+
+main:CreateButton("Button", function() --creates a button
+	print("Hello")
+end)
+
+main:CreateToggle("Toggle", true, function(ToggleState) --creates a toggle
+	if ToggleState == true then
+		print("Enabled.")
+	else
+		print("Disabled.")
+	end
+end)
