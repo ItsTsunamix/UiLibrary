f591b8f3c96f60fa1e6a6cab0d445ba738ec5b55
Pandora37 <83253758+Pandora37@users.noreply.github.com>
2021-11-04 13:51:07 -0400

    Update MoonLibrary.lua

--- MoonLibrary.lua
+++ MoonLibrary.lua
@@ -7,6 +7,7 @@ Changelog:
 
 1.) Sliders are back, got a redesign and now use tweenservice for sliding.
 2.) Added a fix for Dropdowns, ExpandBy function, uses 1 parameter, Integer.
+3.) Added Pause macro, library:Pause(<bool> is_paused)
 
 ]]
 
@@ -261,6 +262,11 @@ function library:Create(Name)
 		end
 		--end)
 	end
+
+    function library:Pause(IsPaused)
+        IsPaused = IsPaused or true
+        game.Players.LocalPlayer.GameplayPaused = IsPaused
+    end
 
 	function library:Close(OpenVisible)
 		OpenVisible = OpenVisible or true
